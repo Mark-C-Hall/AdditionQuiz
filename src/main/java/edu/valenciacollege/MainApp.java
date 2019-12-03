9edc1f9dd94e4b4160f00d920a60d315f36ae1f9
Mark Hall <Mark.Hall993@gmail.com>
2019-12-03 08:20:14 -0500

    Initial Commit

---
+++ src/main/java/edu/valenciacollege/MainApp.java
@@ -0,0 +1,30 @@
+package edu.valenciacollege;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/scene.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+
+        stage.setTitle("Addition Quiz");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/edu/valenciacollege/SceneController.java
@@ -0,0 +1,5 @@
+package edu.valenciacollege;
+
+public class SceneController {
+
+}
