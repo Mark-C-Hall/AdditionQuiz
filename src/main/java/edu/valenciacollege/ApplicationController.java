c9561307be468e2e5ad47a0eef07c58072ca1145
Mark Hall <Mark.Hall993@gmail.com>
2019-12-09 18:37:12 -0500

    Add generic functionality.

--- src/main/java/edu/valenciacollege/ApplicationController.java
+++ src/main/java/edu/valenciacollege/ApplicationController.java
@@ -1,5 +1,6 @@
 package edu.valenciacollege;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
@@ -7,7 +8,7 @@ import javafx.scene.text.Text;
 import java.io.IOException;
 
 public class ApplicationController {
-    @FXML Text nameLabel, timeLabel;
+    @FXML Text nameLabel, timeLabel, pointsLabel;
     @FXML Text objectLabel1, objectLabel2;
     @FXML Button answerLabel1, answerLabel2, answerLabel3, answerLabel4;
     String name;
@@ -27,6 +28,11 @@ public class ApplicationController {
         } catch (IOException e) {
             System.out.println("Caught: " + e.getMessage());
         }
+        bindPoints();
+    }
+
+    private void bindPoints() {
+        pointsLabel.textProperty().setValue(String.valueOf(this.points));
     }
 
     // Starts the first Countdown Thread.
@@ -37,8 +43,12 @@ public class ApplicationController {
 
     // Ends current thread and starts new one.
     private void resetClock() {
-        countdown.stop();
-        startClock();
+        try {
+            countdown.stop();
+            startClock();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     // Method for setting the questions and answer texts.
@@ -51,8 +61,38 @@ public class ApplicationController {
         answerLabel4.textProperty().bindBidirectional(qState.answer4);
     }
 
+    private String calculateAnswer(QuestionAndAnswerState state) {
+        return state.getCorrectAnswer();
+    }
+
+    private void correctAnswer() {
+        points += 1;
+        if (points > 10) {
+            finishQuiz();
+        } else {
+            qState.newQuestion();
+            bindPoints();
+        }
+//        resetClock();
+    }
+
+    private void finishQuiz() {
+        System.out.println("Quiz Finished.");
+    }
+
+    private void incorrectAnswer() {
+        qState.newQuestion();
+//        resetClock();
+    }
+
     // Method for pressing answer button.
-    public void submitAnswer() {
-        resetClock();
+    public void submitAnswer(ActionEvent event) {
+        String answerSelected = ((Button)event.getSource()).getText();
+        String correctAnswer = calculateAnswer(qState);
+        if (answerSelected.equals(correctAnswer)) {
+            correctAnswer();
+        } else {
+            incorrectAnswer();
+        }
     }
 }
--- src/main/java/edu/valenciacollege/QuestionAndAnswerState.java
+++ src/main/java/edu/valenciacollege/QuestionAndAnswerState.java
@@ -1,78 +1,180 @@
 package edu.valenciacollege;
 
 import javafx.beans.property.SimpleStringProperty;
-
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
+/*
+ * Class produces and Question and Answer state. First, text is read from file. Then, the two addition objects are
+ * assigned as well as the four possible answers. The calculated answer is also done in class through generic methods.
+ * The state loops through the 10 questions in random order.
+ */
 public class QuestionAndAnswerState {
+    // JavaFX Properties
     SimpleStringProperty object1 = new SimpleStringProperty(this, "object1");
     SimpleStringProperty object2 = new SimpleStringProperty(this, "object2");
     SimpleStringProperty answer1 = new SimpleStringProperty(this, "answer1");
     SimpleStringProperty answer2 = new SimpleStringProperty(this, "answer2");
     SimpleStringProperty answer3 = new SimpleStringProperty(this, "answer3");
     SimpleStringProperty answer4 = new SimpleStringProperty(this, "answer4");
+
+    // Class Properties
+    private String correctAnswer;
     private int questionNumber = 0;
-    private  ArrayList<ArrayList<String>> listOfQuestionsAndAnswers;
+    private ArrayList<ArrayList<Object>> listOfQuestionsAndAnswers;
+    private Scanner scanner;
 
     QuestionAndAnswerState() throws IOException {
         listOfQuestionsAndAnswers = new ArrayList<>();
+        File file = getFileFromResource();
+        scanner = new Scanner(file);
         listOfQuestionsAndAnswers = getAllQuestionsAndAnswers();
-        setQuestionsAndAnswers(produceQuestionAndAnswerList(listOfQuestionsAndAnswers));
+        setQuestionsAndAnswers(
+                produceQuestionAndAnswerList(
+                        listOfQuestionsAndAnswers));
+    }
+
+    // Load text file from Resource Folder.
+    private File getFileFromResource() {
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        URL resource = classLoader.getResource("input.txt");
+        if (resource == null) {
+            throw new IllegalArgumentException("file is not found!");
+        } else {
+            return new File(resource.getFile());
+        }
     }
 
     /*
-     * This method reads from a file and returns a list of lists. As the file is read, the two addition objects are
-     * randomly selected to the two first indexes of the sublist. Then the next four answer objects are randomly placed
-     * in the 3rd-6th indexes. The 10 sublists are then created and shuffled into the master list of lists.
+     * This method reads from text file and returns a list of lists. As the file is read, the two addition objects are
+     * selected to the two first indexes of the sublist. Then the next four answer objects are randomly placed
+     * in the 3rd-6th indexes. Finally, the correct answer is calculated and appended to the end of the sublist. The 10
+     *  sublists are then added  and shuffled into the master list of lists.
      */
-    private ArrayList<ArrayList<String>> getAllQuestionsAndAnswers() throws IOException {
-        File file = new File("C:\\Users\\Mark\\Documents\\Valencia\\2019-2020\\Fall-2019\\AdvJava\\AdditionQuiz\\src\\main\\resources\\input.txt");
-        Scanner scanner = new Scanner(file);
-        ArrayList<ArrayList<String>> resultList = new ArrayList<>();
+    private ArrayList<ArrayList<Object>> getAllQuestionsAndAnswers() {
+        ArrayList<ArrayList<Object>> resultList = new ArrayList<>();
 
         // Loop through file lines two at a time
         for (int i = 0; i < 10; i++) {
-            ArrayList<String> tempList = new ArrayList<>();  // Temp list to hold the question objects.
-            ArrayList<String> answerList = new ArrayList<>();    // Temp list to hold the answer objects.
+            ArrayList<Object> tempList = new ArrayList<>();  // Temp list to hold the question objects.
+            ArrayList<Object> answerList = new ArrayList<>();    // Temp list to hold the answer objects.
 
             // Question lines.
-            tempList.add(scanner.next());
-            tempList.add(scanner.next());
-            Collections.shuffle(tempList);
+            tempList.add(scanNext());
+            tempList.add(scanNext());
             // Answer lines.
-            answerList.add(scanner.next());
-            answerList.add(scanner.next());
-            answerList.add(scanner.next());
-            answerList.add(scanner.next());
+            answerList.add(scanNext());
+            answerList.add(scanNext());
+            answerList.add(scanNext());
+            answerList.add(scanNext());
             Collections.shuffle(answerList);
 
             // This list contains two questions and four answers
             tempList.addAll(answerList);
+
+            // Append temp list with calculated answers
+            tempList.add(calculateAnswer(tempList));
             resultList.add(tempList);
+
         }
         Collections.shuffle(resultList);
         return resultList;
     }
 
-    private ArrayList<String> produceQuestionAndAnswerList(ArrayList<ArrayList<String>> list)  {
+    // Method for ensuring data types remain constant.
+    private Object scanNext() {
+        if (scanner.hasNextInt()) {
+            return scanner.nextInt();
+        } else if (scanner.hasNextDouble()) {
+            return scanner.nextDouble();
+        } else {
+            // Case handling for float values with "f" suffix
+            if (scanner.hasNext(".*f")) {
+                return Double.valueOf(scanner.next());
+            } else {
+                return scanner.next();
+            }
+        }
+    }
+
+    /*
+     * Generic method for adding two objects. The tree of if/else statements is to see the class types of the incoming
+     * and cast the generic objects to their data types, since string addition, and numerical addition need separte
+     * methods.
+     */
+    private String calculateAnswer(ArrayList<Object> list) {
+        if (list.get(0).getClass() == Integer.class && list.get(1).getClass() == Integer.class) {
+            int obj1 = (Integer)list.get(0);
+            int obj2 = (Integer)list.get(1);
+            return calcInts(obj1, obj2);
+        } else if (list.get(0).getClass() == Double.class || list.get(1).getClass() == Double.class) {
+            if (list.get(0).getClass() == Double.class && list.get(1).getClass() == Double.class) {
+                double obj1 = (Double)list.get(0);
+                double obj2 = (Double)list.get(1);
+                return calcDoubles(obj1, obj2);
+            } else if(list.get(0).getClass() == Double.class && list.get(1).getClass() == Integer.class) {
+                double obj1 = (Double)list.get(0);
+                int obj2 = (Integer) list.get(1);
+                return calcDoubles(obj1, obj2);
+            } else {
+                int obj1 = (Integer) list.get(1);
+                double obj2 = (Double)list.get(0);
+                return calcDoubles(obj1, obj2);
+            }
+        } else {
+            return concatString(list.get(0), list.get(1));
+        }
+    }
+
+    // Helper method for adding ints.
+    private <T extends Number> String calcInts(T o, T o1) {
+        int result = o.intValue() + o1.intValue();
+        return Integer.toString(result);
+    }
+
+    // Helper method for adding doubles. If double and int, then int is cast to double.
+    private <T extends Number> String calcDoubles(T o, T o1) {
+        double result = o.doubleValue() + o1.doubleValue();
+        return Double.toString(result);
+    }
+
+    // Helper method for adding Strings.
+    private <T> String concatString(T o, T o1) {
+        return o.toString().concat(o1.toString());
+    }
+
+    // Method for selecting the question state in list of questions.
+    private ArrayList<Object> produceQuestionAndAnswerList(ArrayList<ArrayList<Object>> list)  {
         return list.get(questionNumber);
     }
 
-    private void setQuestionsAndAnswers(ArrayList<String> list) {
-        this.object1.set(list.get(0));
-        this.object2.set(list.get(1));
-        this.answer1.set(list.get(2));
-        this.answer2.set(list.get(3));
-        this.answer3.set(list.get(4));
-        this.answer4.set(list.get(5));
+    // Method for setting the JavaFX properties to question state.
+    private void setQuestionsAndAnswers(ArrayList<Object> list) {
+        this.object1.set(list.get(0).toString());
+        this.object2.set(list.get(1).toString());
+        this.answer1.set(list.get(2).toString());
+        this.answer2.set(list.get(3).toString());
+        this.answer3.set(list.get(4).toString());
+        this.answer4.set(list.get(5).toString());
+
+        this.correctAnswer = list.get(6).toString();
     }
 
-    private void newQuestion() {
+    // New Question State.
+    void newQuestion() {
         questionNumber += 1;
-        setQuestionsAndAnswers(produceQuestionAndAnswerList(listOfQuestionsAndAnswers));
+        setQuestionsAndAnswers(
+                produceQuestionAndAnswerList(
+                        listOfQuestionsAndAnswers));
+    }
+
+    // Getters
+    public String getCorrectAnswer() {
+        return this.correctAnswer;
     }
 }
